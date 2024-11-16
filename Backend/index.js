bc945db712264e19c0c64e060b0e8d3e63cebcb6
Hemant <hemantnegi708@gmail.com>
2024-11-16 21:39:24 +0530

    final commit

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://careerwave-1.onrender.com",
   credentials: true,
 };
 
